d63656089f29cc07c75606dde5cb29efb65ca1fc
antoshjkeee <toha_bart@mail.ru>
2024-10-15 03:02:32 +0300

    setup v1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+	return (
+		<div className='app'>
+			<header className='App-header'>
+				some header
+			</header>
+			<main>
+				<div>some text</div>
+			</main>
+			<footer>some footer</footer>
+		</div>
+	);
+}
+
+export default App;
